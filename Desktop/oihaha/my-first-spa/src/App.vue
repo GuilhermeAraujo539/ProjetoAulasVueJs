2100f6ba3051768a2c8048f44854c6e7a7a0c6e6
Guilherme Souza <guilherme.souza539@etec.sp.gov.br>
2024-09-20 17:26:29 -0300

    Atualizando Atividade desenvolvida em aula

---
+++ Desktop/oihaha/my-first-spa/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <NavPage></NavPage>
+  <ContentPage></ContentPage>
+  <FooterPage></FooterPage>
+  <LoopComponent></LoopComponent>
+  <ObjectComponent></ObjectComponent>
+</template>
+
+<script>
+import NavPage from './components/default/NavPage.vue';
+import FooterPage from './components/default/FooterPage.vue';
+import ContentPage from './components/default/ContentPage.vue';
+import LoopComponent from './components/default/LoopComponent.vue';
+import ObjectComponent from './components/default/ObjectComponent.vue';
+
+export default{
+  name: 'App',
+  components: {
+    NavPage, 
+    ContentPage,
+    FooterPage,
+    LoopComponent,
+    ObjectComponent
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
