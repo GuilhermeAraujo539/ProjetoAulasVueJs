5bafa40f302f23d7657c2b502674280bc169bf0e
MuriloRiber <you@example.com>
2024-09-27 17:17:08 -0300

    AAAAAAAAAAAA

--- Desktop/oihaha/my-first-spa/src/App.vue
+++ Desktop/oihaha/my-first-spa/src/App.vue
@@ -1,30 +1,20 @@
 <template>
-  <NavPage></NavPage>
-  <ContentPage></ContentPage>
-  <FooterPage></FooterPage>
-  <LoopComponent></LoopComponent>
-  <ObjectComponent></ObjectComponent>
+  <div>
+    <nav>
+      <router-link to="/">Hello World</router-link>
+      <router-link to="/Loop">Loop</router-link>
+    </nav>
+  </div>
+  
+  <router-view></router-view>
 </template>
 
 <script>
-import NavPage from './components/default/NavPage.vue';
-import FooterPage from './components/default/FooterPage.vue';
-import ContentPage from './components/default/ContentPage.vue';
-import LoopComponent from './components/default/LoopComponent.vue';
-import ObjectComponent from './components/default/ObjectComponent.vue';
-
-export default{
+export default {
   name: 'App',
-  components: {
-    NavPage, 
-    ContentPage,
-    FooterPage,
-    LoopComponent,
-    ObjectComponent
-  }
 }
 </script>
 
-<style scoped>
+<style>
 
 </style>
---
+++ Desktop/oihaha/my-first-spa/src/router.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+import LoopComponent from "./components/LoopComponent.vue";
+
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: HelloWorld
+    },
+    {
+        path: '/Loop',
+        name: 'Loop',
+        component: LoopComponent
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+export default router;
